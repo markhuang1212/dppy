a35c5e259cc987603c894d558b330302303957eb
Meng Huang <mark.huang1212@gmail.com>
2021-11-25 04:33:22 +0000

    first commit - project initialization

---
+++ README.md
@@ -0,0 +1,9 @@
+# dppy
+
+Repository for the FYT project
+
+## Installation
+
+* Docker
+* VSCode
+* VSCode - Remote Container Extension
---
+++ smooth_sensitivity.py
@@ -0,0 +1,10 @@
+"""
+This module provide methods to compute differentially private median
+using the smooth sensitivity method
+"""
+
+def median_smooth_sensitivity():
+    """
+    return the dp private median
+    """
+    return []
